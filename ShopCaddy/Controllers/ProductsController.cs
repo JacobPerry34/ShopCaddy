7aba8fb798bf74be82870dc1c13a79e3276003b3
JACOB <JACOBPRY@AOl.com>
2020-02-08 13:44:47 -0500

    Commented a bit more

--- ShopCaddy/Controllers/ProductsController.cs
+++ ShopCaddy/Controllers/ProductsController.cs
@@ -166,7 +166,7 @@ namespace ShopCaddy.Controllers
             var product = await _context.Products.FindAsync(id);
             _context.Products.Remove(product);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Redirect("/ProductTypes");
         }
 
         private bool ProductExists(int id)
